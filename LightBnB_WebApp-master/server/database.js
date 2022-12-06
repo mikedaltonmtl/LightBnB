80a7424856939843a34b7c68ee70337b89a5ee6f
mikedaltonmtl <michael.j.dalton@gmail.com>
2022-12-06 14:01:11 +0000

    install pg and test connection

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,6 +1,19 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 
+// Creaste pool to connect to lightbnb database
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
+// test connection
+pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {console.log(response)});
+
 /// Users
 
 /**
