4bbda285133404efb816b65923fbb45f3cee6017
mikedaltonmtl <michael.j.dalton@gmail.com>
2022-12-13 01:14:46 +0000

    refactor getAllProperties to include search terms

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -133,8 +133,81 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = (options, limit = 10) => {
+  // Check the options object that has been passed
+  console.log('options', options);
+  // Setup an array to hold any parameters that may be available for the query.
+  const queryParams = [];
+  // Use a boolean to track whether this is the first WHERE clause
+  let multipleWheres = false;
+  // Start the query with all information that comes before the WHERE clause.
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+  /*
+  Check if a city has been passed in as an option.
+  Add the city to the params array and create a WHERE clause for the city.
+  We can use the length of the array to dynamically get the $n placeholder number.
+  Since this is the first parameter, it will be $1.
+  The % syntax for the LIKE clause must be part of the parameter, not the query.
+  */
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+    multipleWheres = true;
+  }
+  // If an owner_id is passed in, only return properties belonging to that owner.
+  if (options.owner_id) {
+    queryParams.push(options.owner_id);
+    queryString += multipleWheres ? `AND ` : 'WHERE ';
+    multipleWheres = true;
+    queryString += `owner_id = $${queryParams.length} `;
+  }
+  // Check for minimum & maximum cost parameters... both are passed
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryString += multipleWheres ? `AND ` : 'WHERE ';
+    multipleWheres = true;
+    queryParams.push(options.minimum_price_per_night, options.maximum_price_per_night);
+    queryString += `cost_per_night BETWEEN $${queryParams.length - 1} AND $${queryParams.length} `;
+  }
+  // Only by minimum price per night
+  if (options.minimum_price_per_night && !options.maximum_price_per_night) {
+    queryString += multipleWheres ? `AND ` : 'WHERE ';
+    multipleWheres = true;
+    queryParams.push(options.minimum_price_per_night);
+    queryString += `cost_per_night >= $${queryParams.length} `;
+  }
+  // Only by maximum price per night
+  if (!options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryString += multipleWheres ? `AND ` : 'WHERE ';
+    multipleWheres = true;
+    queryParams.push(options.maximum_price_per_night);
+    queryString += `cost_per_night <= $${queryParams.length} `;
+  }
+
+  // Add any query that comes after the WHERE clause.
+  queryString += `
+  GROUP BY properties.id 
+  `;
+  // If a minimum_rating is passed in, only return properties with a rating equal to or higher than that.
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += `HAVING avg(property_reviews.rating) >= $${queryParams.length} `;
+  }
+  // Lastly, set the limit for the number of results to be returned
+  queryParams.push(limit);
+  queryString += `
+  ORDER BY cost_per_night 
+  LIMIT $${queryParams.length};
+  `;
+
+  // Console log everything just to make sure we've done it right.
+  console.log(queryString, queryParams);
+
+  // Run the query.
   return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .query(queryString, queryParams)
     .then((result) => {
       // console.log(result.rows);
       return result.rows;
@@ -144,7 +217,6 @@ const getAllProperties = (options, limit = 10) => {
     });
 };
 exports.getAllProperties = getAllProperties;
-
 
 /**
  * Add a property to the database
